6b5bd92291a44e45319c7b66dbc0c3ba86153ac8
Your Name <you@example.com>
2023-01-26 19:31:40 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -37,10 +37,37 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+    let splitArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        splitArray.push(expr.slice(i, i + 10));
+    }
+
+    let arrayMorse = splitArray.map(item => {
+        let morse = '';
+        for (let i = 0; i < item.length; i += 2) {
+            if (item[i] == '1' && item[i + 1] == '1') {
+                morse += '-';
+            }
+            if (item[i] == '1' && item[i + 1] == '0') {
+                morse += '.';
+            }
+        }
+         return morse;
+    });
+    
+    let res = arrayMorse.map(item => {
+        return item == '' ? ' ' : MORSE_TABLE[item];
+    });
+
+    return res.join('');
+
+    
 }
+
 
 module.exports = {
     decode
 }
+
